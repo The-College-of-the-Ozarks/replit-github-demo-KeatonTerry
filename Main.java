1b34b184767778700621231f61d586adbf5fbe5d
KeatonTerry <112203491+KeatonTerry@users.noreply.github.com>
2023-04-28 09:51:10 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,36 @@
-import java.util.Scanner;
-class Main {
+import java.util.*;
+class Main 
 {   
    public static void main(String[] args) 
    {  
       final int QUARTS_IN_GALLON = 4;
-      int quartsNeeded = 18;
-      int gallonsNeeded;
+      int quartsNeeded;
+      int gallonsNeeded = 18;
       int extraQuartsNeeded;
       Scanner input = new Scanner(System.in);
-      System.out.print("Enter quarts needed >> ");
-      quartsNeeded = input.nextInt();
+      
+      
+      while(true){
+        quartsNeeded = Integer.parseInt(input.next());
+        
+        try{
+          
+          System.out.print("Enter quarts needed >> ");
+        }
+        catch(NumberFormatException e) {
+          System.out.println("Please enter a number >> ");
+          
+          continue;
+        }
       gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
       extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
       System.out.println("A job that needs " + quartsNeeded +
          " quarts requires " + gallonsNeeded + " gallons plus " +
-         extraQuartsNeeded + " quarts.");
+      extraQuartsNeeded + " quarts.");
+      }
+
+        
+      }
+    
+   
    }
-}
